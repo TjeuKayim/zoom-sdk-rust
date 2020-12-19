cbee5498fd4008de492d9ad7748c4da9059ad432
Tjeu Kayim <15987676+TjeuKayim@users.noreply.github.com>
2020-12-19 22:12:24 +0100

    Implement zoom_version()

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,28 @@
+use zoom_sdk_windows_sys as ffi;
+use std::ffi::OsString;
+use std::os::windows::prelude::*;
+
+pub fn zoom_version() -> String {
+    unsafe {
+        let version = ffi::ZOOMSDK_GetVersion();
+        let version = u16_ptr_to_string(version);
+        version.into_string().unwrap()
+    }
+}
+
+unsafe fn u16_ptr_to_string(ptr: *const u16) -> OsString {
+    let len = (0..).take_while(|&i| *ptr.offset(i) != 0).count();
+    let slice = std::slice::from_raw_parts(ptr, len);
+    OsString::from_wide(slice)
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn zoom_version_equals() {
+        let version = zoom_version();
+        assert_eq!("5.2.1 (42037.1112)", &version);
     }
 }
