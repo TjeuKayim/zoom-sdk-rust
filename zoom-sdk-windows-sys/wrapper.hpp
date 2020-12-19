04df3b46f12a5ee5bf60f63d6a99e9b3c8deded4
Tjeu Kayim <15987676+TjeuKayim@users.noreply.github.com>
2020-12-19 18:01:33 +0100

    Build succeeds

--- zoom-sdk-windows-sys/wrapper.hpp
+++ zoom-sdk-windows-sys/wrapper.hpp
@@ -1,2 +1,10 @@
 // #include <user32>
+// #include <vcruntime.h>
+// #include <Windows.h>
+// #include <ntdef.h>
+// #include <winnt.h>
+// #include <windef.h>
+// #define _WINBASE_
+#define _SYSINFOAPI_H_ // conflicts with GetVersion()
+#include <wtypes.h>
 #include <zoom_sdk.h>
