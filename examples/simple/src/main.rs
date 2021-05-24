48c353ac2e20b6bb845861668a71b7616de44ac1
Tjeu Kayim <15987676+TjeuKayim@users.noreply.github.com>
2021-05-24 20:41:04 +0200

    Use Rc again for state

--- examples/simple/src/main.rs
+++ examples/simple/src/main.rs
@@ -14,7 +14,6 @@ fn main() {
 
     nwg::Window::builder()
         .size((300, 115))
-        // .position((300, 300))
         .title("Rust Zoom SDK")
         .build(&mut window)
         .unwrap();
@@ -36,7 +35,7 @@ fn main() {
     let events_window = window.clone();
 
     // TODO: unfortunately, full_bind_event_handler requires static lifetime
-    let zoom_state = RefCell::new(ZoomState::default());
+    let zoom_state = Rc::new(RefCell::new(ZoomState::default()));
 
     let handler = nwg::full_bind_event_handler(&window.handle, move |evt, _evt_data, handle| {
         use nwg::Event as E;
@@ -60,14 +59,12 @@ fn main() {
 }
 
 #[derive(Debug, Default)]
-struct ZoomState<'a> {
-    meeting: Option<zoom_sdk::meeting::MeetingService<'a>>,
-    auth: Option<Pin<Box<zoom_sdk::auth::AuthService<'a>>>>,
+struct ZoomState {
+    meeting: Option<zoom_sdk::meeting::MeetingService<'static>>,
+    auth: Option<Pin<Box<zoom_sdk::auth::AuthService<'static>>>>,
 }
 
-fn join_meeting<'a, 'b>(
-    state_cell: &'a RefCell<ZoomState<'b>>,
-) -> Result<(), Box<dyn std::error::Error>> {
+fn join_meeting(state: &Rc<RefCell<ZoomState>>) -> Result<(), Box<dyn std::error::Error>> {
     let init_param = zoom_sdk::InitParam::new()
         .branding_name(Some("RustWrapper")) // working
         .res_instance(unsafe { GetModuleHandleA(ptr::null()) })
@@ -78,10 +75,11 @@ fn join_meeting<'a, 'b>(
         .enable_generate_dump(true);
     zoom_sdk::init_sdk(&init_param).expect("Initialization failed");
     println!("Initialized");
-    let mut state = state_cell.borrow_mut();
-    state.meeting = Some(zoom_sdk::create_meeting_service()?);
-    state.auth = Some(zoom_sdk::create_auth_service()?);
-    let auth = state.auth.as_mut().unwrap();
+    let mut state_borrow = state.borrow_mut();
+    state_borrow.meeting = Some(zoom_sdk::create_meeting_service()?);
+    state_borrow.auth = Some(zoom_sdk::create_auth_service()?);
+    let auth = state_borrow.auth.as_mut().unwrap();
+    let state_clone = state.clone();
     auth.set_event(zoom_sdk::auth::AuthServiceEvent {
         authentication_return: Box::new(|auth, res| {
             catch_error(|| {
@@ -92,19 +90,16 @@ fn join_meeting<'a, 'b>(
                 Ok(())
             });
         }),
-        login_return: Box::new(|_auth, status| {
+        login_return: Box::new(move |_auth, status| {
             catch_error(|| {
                 println!("login status {:?}", status);
                 if let zoom_sdk::auth::LoginStatus::Success(info) = status {
                     let name = info.get_display_name();
                     println!("name {}", name);
                     let uri = std::env::var("ZOOM_URI")?;
-                    // TODO: meeting
-                    // RefCell::borrow_mut(&state_cell)
-                    //     .meeting
-                    //     .as_ref()
-                    //     .unwrap()
-                    //     .handle_zoom_web_uri_protocol_action(&uri)?;
+                    let state = RefCell::borrow_mut(&state_clone);
+                    let meeting = state.meeting.as_ref().unwrap();
+                    meeting.handle_zoom_web_uri_protocol_action(&uri)?;
                 }
                 Ok(())
             });
@@ -118,15 +113,5 @@ fn join_meeting<'a, 'b>(
 fn catch_error(f: impl FnOnce() -> Result<(), Box<dyn std::error::Error>>) {
     f().unwrap_or_else(|e| {
         eprintln!("{0}, detail {0:?}", &e);
-        // self.report_error(&format!("{}", &e));
     });
 }
-
-// fn init_status(&self, text: &str) {
-//     self.init_status_label.set_text(text);
-// }
-//
-// fn report_error(&self, err_message: &str) {
-//     nwg::modal_error_message(&self.window, "Error", err_message);
-// }
-//
